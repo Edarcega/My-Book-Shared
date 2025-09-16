7278e80af940f66591facd86803a36f739c94fd1
edimar <edimar.eds@gmail.com>
2025-09-16 07:56:37 -0300

    first commit

---
+++ src/main/java/com/edstecno/My/Book/Shared/MyBookSharedApplication.java
@@ -0,0 +1,13 @@
+package com.edstecno.My.Book.Shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookSharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookSharedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edstecno/My/Book/Shared/MyBookSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstecno.My.Book.Shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookSharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
